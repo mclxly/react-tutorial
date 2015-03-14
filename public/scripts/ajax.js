6db8dab79d09906b64f0738e3ecf6c75fab87368
mclxly <mclxly@gmail.com>
2015-03-14 17:02:31 +0800

    ajax

---
+++ public/scripts/ajax.js
@@ -0,0 +1,101 @@
+var UserGist = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      lastGistUrl: ''
+    };
+  },
+
+  componentDidMount: function() {
+    $.get(this.props.source, function(result) {
+      var lastGist = result[0];
+      if (this.isMounted()) {
+        this.setState({
+          username: lastGist.owner.login,
+          lastGistUrl: lastGist.html_url
+        });
+      }
+    }.bind(this));
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.state.username}'s last gist is
+        <a href={this.state.lastGistUrl}>here</a>.
+      </div>
+    );
+  }
+});
+
+React.render(
+  <UserGist source="https://api.github.com/users/octocat/gists" />,
+  document.getElementById('content')
+);
+
+// -----------------------------------------
+var TodoList = React.createClass({
+  getInitialState: function() {
+    return {
+      id: '0',
+      title: '',
+      done: false,
+      created_at: '',
+      updated_at: ''
+    };
+  },
+
+  componentDidMount: function() {
+    $.ajax({
+        type: "GET",
+        dataType: 'json',
+        url: this.props.source,
+        crossDomain : true
+    })
+      .done(function( result ) {
+          console.log("done");
+
+          var lastGist = result[0];
+          if (this.isMounted()) {
+            this.setState({
+              id: lastGist.id,
+              title: lastGist.title,
+              done: lastGist.done,
+              created_at: lastGist.created_at,
+              updated_at: lastGist.updated_at
+            });
+          }
+      }.bind(this))
+      .fail( function(xhr, textStatus, errorThrown) {
+          alert(xhr.responseText);
+          alert(textStatus);
+      });
+    /*
+    $.get(this.props.source, function(result) {
+      var lastGist = result[0];
+      if (this.isMounted()) {
+        this.setState({
+          id: lastGist.id,
+          title: lastGist.title,
+          done: lastGist.done,
+          created_at: lastGist.created_at,
+          updated_at: lastGist.updated_at
+        });
+      }
+    }.bind(this), 'jsonp');*/
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.state.title}'s last gist is
+        {this.state.created_at} - {this.state.updated_at}
+      </div>
+    );
+  }
+});
+
+React.render(
+  <TodoList source="http://192.168.21.90:8000/api/todos" />,
+  document.getElementById('example')
+);
