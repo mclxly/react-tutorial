ded7c678a75567c0028db73958980395b0245697
mclxly <mclxly@gmail.com>
2015-03-17 17:04:00 +0800

    daily update

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -99,3 +99,65 @@ React.render(
   <TodoList source="http://192.168.21.90:8000/api/todos" />,
   document.getElementById('example')
 );
+
+// ---------------------------------------
+var Filters = React.createClass({
+  handleFilterChange: function() {
+    var value = this.refs.filterInput.getDOMNode().value;
+    this.props.updateFilter(value);
+  },
+  render: function() {
+    return <input type="text" ref="filterInput" onChange={this.handleFilterChange} placeholder="Filter" />;
+  }
+});
+
+var List = React.createClass({
+  render: function() {
+    var content;
+    if (this.props.items.length > 0) {
+      var items = this.props.items.map(function(item) {
+        return <li>{item}</li>;
+      });
+      content = <ul>{items}</ul>
+    } else {
+      content = <p>No items matching this filter</p>;
+    }
+    return (
+      <div className="results">
+        <h4>Results</h4>
+        {content}
+      </div>
+    );
+  }
+});
+
+var ListContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      listItems: ['Chicago', 'New York', 'Tokyo', 'London', 'San Francisco', 'Amsterdam', 'Hong Kong'],
+      nameFilter: ''
+    };
+  },
+  handleFilterUpdate: function(filterValue) {
+    this.setState({
+      nameFilter: filterValue
+    });
+  },
+  render: function() {
+    var displayedItems = this.state.listItems.filter(function(item) {
+      if (this.state.nameFilter == '') return true;
+      
+      var match = item.toLowerCase().indexOf(this.state.nameFilter.toLowerCase());
+      return (match !== -1);
+    }.bind(this));
+
+    return (
+      <div>
+        <Filters updateFilter={this.handleFilterUpdate} />
+        <List items={displayedItems} />
+      </div>
+    );
+  }
+});
+
+React.renderComponent(<ListContainer />, document.getElementById('example'));
