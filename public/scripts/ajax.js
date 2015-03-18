bc5f3d803905be8e7c3567b0049693de02f8bb47
mclxly <mclxly@gmail.com>
2015-03-18 17:07:55 +0800

    daily update

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -101,63 +101,21 @@ React.render(
 );
 
 // ---------------------------------------
-var Filters = React.createClass({
-  handleFilterChange: function() {
-    var value = this.refs.filterInput.getDOMNode().value;
-    this.props.updateFilter(value);
+function createMarkup() { return {__html: 'First &middot; Second'}; };
+
+var App = React.createClass({
+  componentDidMount: function() {
+    // This doesn't refer to the `span`s! It refers to the children between
+    // last line's `<App></App>`, which are undefined.
+    console.log(this.props.children);
   },
+
   render: function() {
-    return <input type="text" ref="filterInput" onChange={this.handleFilterChange} placeholder="Filter" />;
+
+   return <div dangerouslySetInnerHTML={createMarkup()} />
+    return <div><span/><span/></div>;
   }
 });
 
-var List = React.createClass({
-  render: function() {
-    var content;
-    if (this.props.items.length > 0) {
-      var items = this.props.items.map(function(item) {
-        return <li>{item}</li>;
-      });
-      content = <ul>{items}</ul>
-    } else {
-      content = <p>No items matching this filter</p>;
-    }
-    return (
-      <div className="results">
-        <h4>Results</h4>
-        {content}
-      </div>
-    );
-  }
-});
 
-var ListContainer = React.createClass({
-  getInitialState: function() {
-    return {
-      listItems: ['Chicago', 'New York', 'Tokyo', 'London', 'San Francisco', 'Amsterdam', 'Hong Kong'],
-      nameFilter: ''
-    };
-  },
-  handleFilterUpdate: function(filterValue) {
-    this.setState({
-      nameFilter: filterValue
-    });
-  },
-  render: function() {
-    var displayedItems = this.state.listItems.filter(function(item) {
-      if (this.state.nameFilter == '') return true;
-      
-      var match = item.toLowerCase().indexOf(this.state.nameFilter.toLowerCase());
-      return (match !== -1);
-    }.bind(this));
-
-    return (
-      <div>
-        <Filters updateFilter={this.handleFilterUpdate} />
-        <List items={displayedItems} />
-      </div>
-    );
-  }
-});
-
-React.renderComponent(<ListContainer />, document.getElementById('example'));
+React.renderComponent(<App><a></a></App>, document.getElementById('example'));
